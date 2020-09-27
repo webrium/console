28b4e2a949f89e2bac49175d7a22b55b7ef5563f
سید بنیامین خلیفه <nvioir@gmail.com>
2020-09-28 00:08:12 +0330

    download bootstrap file and extract

--- src/DownloadUIBootstrap.php
+++ src/DownloadUIBootstrap.php
@@ -14,62 +14,107 @@ use webrium\component\Download;
 class DownloadUIBootstrap extends Command
 {
     // the name of the command (the part after "bin/console")
-    protected static $defaultName = 'ui:bootstrap-install';
+    protected static $defaultName = 'ui:bootstrap';
 
     protected function configure()
     {
         Directory::initDefaultStructure();
-        $this
-            ->addArgument('version', InputArgument::OPTIONAL, 'Who do you want to greet?');
+        $this->addArgument('action', InputArgument::REQUIRED, '');
+        $this->addArgument('arg', InputArgument::OPTIONAL, '');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $action = $input->getArgument('action');
+        $arg      = $input->getArgument('arg');
 
-        $v = $input->getArgument('version');
+        if($action=='install'){
+            return $this->install($arg,$output);
+        }
+        elseif($action=='init-view'){
+            return $this->initView($arg,$output);
+        }
 
+
+        
+    }
+
+    private function initView($name,$output){
+        $html = '<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="@url(\'library/bootstrap/css/bootstrap.min.css\')">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="@url(\'library/bootstrap/js/bootstrap.min.js\')"></script>
+  </body>
+</html>';
+
+        $s = $output->section();
+
+        $view_path = Directory::path('views');
+
+        $s->writeln('Creating a view file ..');
+
+        File::putContent("$view_path/$name.php",$html);
+
+        sleep(2);
+
+        $s->overwrite("View created");
+        $s->writeln("path : <info>$view_path/$name.php</info>");
+        return Command::SUCCESS;
+    }
+
+    private function install($v=false,$output){
 
         $url= 'https://github.com/twbs/bootstrap/releases/download/v4.5.2/bootstrap-4.5.2-dist.zip';
         $file_name = basename($url);
 
-        $s = $output->section();
+        $save_path = Directory::path('storage').'/temp';
+        Directory::make($save_path);
 
-        $s->writeln("Downloading ". $file_name);
+        $output->writeln("Downloading $file_name\n");
 
-        $status = Download::url($url,false,$save_path);
+        $status = Download::url($url,$save_path);
 
-        if($status != 200){
+        // if($status != 200){
 
-        }
+        // }
 
-        $zip = new ZipArchive;
-        if ($zip->open('test.zip') === TRUE) {
-            $zip->extractTo('/my/destination/dir/');
+        $extract_to = Directory::path('public')."/library/";
+        
+        $output->writeln("Extract $file_name to $extract_to");
+
+        $zip = new \ZipArchive;
+        if ($zip->open("$save_path/$file_name") === TRUE) {
+            $zip->extractTo($extract_to);
             $zip->close();
-            echo 'ok';
+            rename("$extract_to/bootstrap-4.5.2-dist","$extract_to/bootstrap");
         } else {
-            echo 'failed';
         }
 
-        $zip = new ZipArchive;
-        $res = $zip->open("$save_path/$file_name");
+        File::delete("$save_path/$file_name");
+        $output->writeln("Clear temp files");
+        $output->writeln("----------------\n");
 
-        if ($res === TRUE) {
-            $zip->extractTo(Directory::path('public').'/library/');
-            $zip->close();
-            echo 'woot!';
-        } else {
-            echo 'doh!';
-        }
-
-
-       // $s->overwrite("Model created");
+        
+        $output->writeln("<info>Installation completed</info>");
 
         return Command::SUCCESS;
-    }
 
-    private function getBasicStr($name){
-        return "<?php\nnamespace app\models;\n\nclass $name{\n//..\n\n}";
     }
 
    
