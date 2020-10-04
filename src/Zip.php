8cda0197d5f1bb49541d659b6b369762697dc0c4
سید بنیامین خلیفه <nvioir@gmail.com>
2020-10-04 16:01:27 +0330

    add zip class

---
+++ src/Zip.php
@@ -0,0 +1,19 @@
+<?php
+namespace webrium\component;
+
+class Zip{
+
+  public function extract($zip_path,$extract_to)
+  {
+    $zip = new \ZipArchive;
+    if ($zip->open($zip_path) === TRUE) {
+        $zip->extractTo($extract_to);
+        $zip->close();
+        return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+}
