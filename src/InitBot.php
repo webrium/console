b9bb1d51d68057c5ef20b46c170a1da846744855
parsgit <nvioir@gmail.com>
2023-07-25 20:00:10 +0000

    Add botfire:init command

--- src/InitBot.php
+++ src/InitBot.php
@@ -6,39 +6,29 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 use Webrium\File;
+use Webrium\App;
 use Webrium\Directory;
 
 class InitBot extends Command
 {
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'botfire:init';
+    protected static $defaultDescription = 'Creates files and initial values to start Telegram bot';
 
     protected function configure()
     {
         Directory::initDefaultStructure();
         $this
-            ->addArgument('token', InputArgument::OPTIONAL, 'Who do you want to greet?');
+            ->addArgument('token', InputArgument::OPTIONAL, 'Your Telegram bot token');
+            $this->addOption('debug',null,InputOption::VALUE_REQUIRED, 'It activates the debug mode. Also, you must enter the ID of the chat account to which you want to send error messages.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $token = $input->getArgument('token');
-
-        // $output->writeln("<error>Modles directory not exists\n$model_dir</error>");
-
-        
-        // $s = $output->section();
-
-        // $s->writeln('Creating a model file ..');
-
-        // File::putContent("$model_dir/$name.php", $this->getBasicStr($name));
-
-        // sleep(2);
-
-        // $s->overwrite("Model created");
-        // $s->writeln("path : <info>$model_dir/$name.php</info>");
 
         $app_routes_dir = Directory::path('routes');
         $app_controllers_dir = Directory::path('controllers');
@@ -50,6 +40,75 @@ class InitBot extends Command
         copy("$dir_bot/BotController.php", "$app_controllers_dir/BotController.php" );
         copy("$dir_bot/BotWelcomeController.php", "$app_controllers_dir/BotWelcomeController.php" );
 
+        $output->writeln('');
+        $output->writeln("'Bot.php' file was added to Routes Directory");
+        $output->writeln("'BotController.php' file was added to Controllers Directory");
+        $output->writeln("'BotWelcomeController.php' file was added to Controllers Directory");
+        $output->writeln('');
+
+        
+        if(empty($token)==false){
+
+            $env = File::getContent(App::rootPath('.env'));
+            $env_array = explode("\n", $env);
+    
+            $find_bot_token = false;
+            $find_bot_debug = false;
+            $find_bot_debug_id = false;
+
+            foreach ($env_array as $key => &$line) {
+                if(strpos($line, 'bot_token')!==false){
+                    $find_bot_token = true;
+                }
+
+                if(strpos($line, 'bot_debug') !== false ){
+                    $find_bot_debug = true;
+                }
+
+                if(strpos($line, 'bot_debug_chat_id') !== false ){
+                    $find_bot_debug_id = true;
+                }
+            }
+
+            if($find_bot_token == false){
+                $env_array[] = "bot_token = $token";
+                $output->writeln("Added bot token in '.env' file.");
+            }
+            else{
+                $output->writeln("The bot_token value already exists in the .env file.");
+            }
+
+            if($find_bot_debug == false){
+                if(empty($input->getOption('debug'))==false){
+                    $env_array[] = "bot_debug = true";
+                    $output->writeln("Debug mode of the bot is activated.");
+
+
+                    if($find_bot_debug_id == false){
+                        $env_array[] = "bot_debug_chat_id = ".$input->getOption('debug');
+                        $find_bot_debug_id = true;
+                        $output->writeln("Added debugger chat id.");
+                    }
+                }
+                else{
+                    $env_array[] = "bot_debug = false";
+                }
+                
+            }
+
+            if($find_bot_debug_id == false){
+                $env_array[] = "bot_debug_chat_id = ";
+            }
+
+ 
+
+            $env_str = implode("\n", $env_array);
+            File::putContent(App::rootPath('.env'), $env_str);
+
+        }
+
+        $output->writeln('');
+        $output->writeln("Bot initialization completed.");
         return Command::SUCCESS;
     }
 
