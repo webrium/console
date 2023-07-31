6b86e0d06f1d0ec268390563bc16c2b5ddac8b76
parsgit <nvioir@gmail.com>
2023-07-31 12:43:02 +0000

    add Models sample files

---
+++ src/Files/Framework/DbModel.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+
+use Foxdb\Model;
+
+class DbModel extends Model{
+
+  /**
+  * The table associated with the model.
+  *
+  * @var string
+  */
+  protected $table = 'model_db';
+
+
+}
