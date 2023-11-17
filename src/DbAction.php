c1a3bbf098148e9a8b175274e1e89f7e84843e5d
parsgit <nvioir@gmail.com>
2023-11-17 09:12:01 +0000

    show databases and create database by console

--- src/DbAction.php
+++ src/DbAction.php
@@ -21,6 +21,7 @@ class DbAction extends Command
     {
         Directory::initDefaultStructure();
         $this->addArgument('action', InputArgument::REQUIRED);
+        $this->addArgument('name', InputArgument::OPTIONAL);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -28,20 +29,46 @@ class DbAction extends Command
         $action = $input->getArgument('action');
 
         if($action == 'tables'){
-            $this->showAllTables($output);
+           return $this->showAllTables($output);
         }
+        else if($action == 'list'){
+            return $this->showAllDb($output);
+        }
+        else if($action == 'create'){
+            return $this->createNewTable($input, $output);
+        }
+
+    }
+
+
+    private function showAllDb($output){
+        $res = DB::query('SHOW DATABASES;', [], true);
+        $res = json_decode(json_encode($res), true);
+        $table = new Table($output);
+        $table->setHeaders(['Databases name']);
+        $table->setRows($res);
+        $table->render();
 
         return Command::SUCCESS;
     }
 
     private function showAllTables($output){
-        $res = DB::query('show tables;', [], true);
+        $res = DB::query('SHOW TABLES;', [], true);
         $res = json_decode(json_encode($res), true);
 
         $table = new Table($output);
         $table->setHeaders(['Table name']);
         $table->setRows($res);
         $table->render();
+
+        return Command::SUCCESS;
+    }
+
+    private function createNewTable($input,$output){
+        $name = $input->getArgument('name');
+        DB::query("CREATE DATABASE IF NOT EXISTS `$name`");
+
+        return Command::SUCCESS;
     }
 
 }
