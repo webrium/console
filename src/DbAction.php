4cf350a8372b946aae23808727f40e3c066b1046
parsgit <nvioir@gmail.com>
2023-11-17 09:56:28 +0000

    Ability to drop database

--- src/DbAction.php
+++ src/DbAction.php
@@ -6,7 +6,9 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 use Foxdb\DB;
 use Webrium\Directory;
@@ -22,6 +24,7 @@ class DbAction extends Command
         Directory::initDefaultStructure();
         $this->addArgument('action', InputArgument::REQUIRED);
         $this->addArgument('name', InputArgument::OPTIONAL);
+        $this->addOption('use', 'u', InputOption::VALUE_OPTIONAL,'Use of custom database');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
@@ -29,13 +32,16 @@ class DbAction extends Command
         $action = $input->getArgument('action');
 
         if($action == 'tables'){
-           return $this->showAllTables($output);
+           return $this->showAllTables($input, $output);
         }
         else if($action == 'list'){
             return $this->showAllDb($output);
         }
         else if($action == 'create'){
             return $this->createNewTable($input, $output);
+        }
+        else if($action == 'drop'){
+            return $this->dropDatabase($input, $output);
         }
 
     }
@@ -52,7 +58,14 @@ class DbAction extends Command
         return Command::SUCCESS;
     }
 
-    private function showAllTables($output){
+    private function showAllTables($input, $output){
+
+        $use = $input->getOption('use');
+
+        if($use){
+            DB::useOnce($use);
+        }
+
         $res = DB::query('SHOW TABLES;', [], true);
         $res = json_decode(json_encode($res), true);
 
@@ -66,7 +79,30 @@ class DbAction extends Command
 
     private function createNewTable($input,$output){
         $name = $input->getArgument('name');
-        DB::query("CREATE DATABASE IF NOT EXISTS `$name`");
+        $status = DB::query("CREATE DATABASE IF NOT EXISTS `$name`");
+       
+        if($status){
+            $output->writeln("<info>The $name database was created</info>");
+        }
+
+        return Command::SUCCESS;
+    }
+
+    private function dropDatabase($input, $output){
+
+        $name = $input->getArgument('name');
+
+        $helper = $this->getHelper('question');
+        $question = new ConfirmationQuestion('<question>Are you sure you want to delete the users database?(Type yes to continue):</question>', false);
+        if (!$helper->ask($input, $output, $question)) {
+            return Command::SUCCESS;
+        }
+        else{
+            DB::query("DROP DATABASE IF EXISTS `$name`");
+            $output->writeLn("<info>$name was deleted<info>");
+        }
+
+
 
         return Command::SUCCESS;
     }
