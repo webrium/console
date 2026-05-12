f0f6cfa6d1ee4ff8fd991e1d360c1605196aab5a
Benyamin Khalife <nvioir@gmail.com>
2026-05-12 10:14:04 +0000

    refactor: streamline InitWebrium command structure and enhance output formatting

--- src/InitWebrium.php
+++ src/InitWebrium.php
@@ -1,33 +1,37 @@
 <?php
 namespace Webrium\Console;
 
-
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
-
-use Webrium\File;
-use Webrium\App;
+use Symfony\Component\Console\Style\SymfonyStyle;
 use Webrium\Directory;
 
 class InitWebrium extends Command
 {
-    // the name of the command (the part after "bin/console")
-    protected static $defaultName = 'init';
-    protected static $defaultDescription = 'Perform initial configuration and create project structure (directories)';
+    protected static $defaultName        = 'init';
+    protected static $defaultDescription = 'Create the project directory structure';
 
-    protected function configure()
-    {        
-    }
+    protected function configure() {}
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $io = new SymfonyStyle($input, $output);
 
-        Directory::makes();
+        $dirs =Directory::makes();
+
+        $io->title('Project Initialization');
+        $io->writeln('<fg=green>✔ Project structure created successfully.</>');
+        $io->newLine();
+
+        
+        foreach ($dirs as $key => $path) {
+            $io->writeln("  <fg=cyan>" . ($key + 1) . "</>  →  ".Directory::path($path));
+        }
+
+        $io->newLine();
+        $io->success('Run "php webrium list" to see all available commands.');
+
         return Command::SUCCESS;
     }
-
-   
 }
