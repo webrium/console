92cabda27cd5e1a36647982dfe05c011fa95ea9e
benkhalife <nvioir@gmail.com>
2026-06-15 15:33:17 +0330

    chore: remove Telegram bot scaffolding (InitBot command and bot template files)

--- src/Files/Bot/Bot.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use botfire\botfire\Route;
-use botfire\botfire\bot;
-
-Route::text('/start','BotWelcomeController->sendHello');
-Route::text('test','BotWelcomeController->sendHello');
-
-Route::notFound('BotWelcomeController->notfound');
