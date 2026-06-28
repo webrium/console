0747fe14c7d2b1bb50667b39bca01a507963efb7
benkhalife <nvioir@gmail.com>
2026-06-28 18:11:01 +0330

    feat(seeder): add stub template for generated seeder files

---
+++ src/Files/Framework/SeederFile.php
@@ -0,0 +1,23 @@
+<?php
+
+use Foxdb\DB;
+use Foxdb\Seeders\Seeder;
+
+class SeederClass extends Seeder
+{
+    /**
+     * Run the seeder.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        // DB::table('table_name')->insert([
+        //     'name'  => 'Example',
+        //     'email' => 'example@example.com',
+        // ]);
+
+        // To call other seeders:
+        // $this->call(AnotherSeeder::class);
+    }
+}
